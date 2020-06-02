7713948620cca3670e4daf410648a49bea6bef2c
davieliang <65671773+davieliang@users.noreply.github.com>
2020-06-02 18:19:05 -0400

    Update Rational.java

--- Rational.java
+++ Rational.java
@@ -5,12 +5,14 @@ public class Rational {
 
   // constructors
 
-  public Rational(int numerator) {
-    // Your code here
-  }
-
   public Rational(int numerator, int denominator) {
-    // Your code here
+    if(denominator == 0)
+      denominator = 1;
+    if(denominator < 0)
+      numerator = numberator *-1;
+      denominator = denominator *-1;
+      
+      reduce()
   }
 
   // getters
@@ -26,17 +28,27 @@ public class Rational {
   // instance methods
 
   public Rational plus(Rational other) {
-    // Your code here
+    
   }
 
   public static Rational plus(Rational a, Rational b) {
-    // Your code here
+    int commomDenominator = denominator *getDenominator();
+    Rational a= numerator*getDenominator();
+    Rational b=getNumerator()*denominator;
+    Rational plus = numerator1 + numerator2;
+    
+    return new RationalNumber(plus, commonDenominator);
   }
 
   // Transforms this number into its reduced form
 
   private void reduce() {
-    // Your code here
+    if(numerator != 0)
+    {
+      int common=gcd(Math.abs(numerator), denominator);
+      
+      numerator = numerator / common;
+      denominator = denominator / common;
   }
 
   // Euclid's algorithm for calculating the greatest common divisor
@@ -56,19 +68,30 @@ public class Rational {
   }
 
   public int compareTo(Rational other) {
-    // Your code here
+    Rational a = this;
+        int lhs = a.num * b.den;
+        int rhs = a.den * b.num;
+        if (lhs < rhs) return -1;
+        if (lhs > rhs) return +1;
+        return 0;
   }
 
   public boolean equals(Rational other) {
-    // Your code here
+    return( numerator == getNumerator()&& denominator == getDenominator() );
   }
 
   public String toString() {
     String result;
     if (denominator == 1) {
-        // Your code here
-    } else {
-        // Your code here
+        result = numerator + "";
+      
+      
+    else {
+     if(denominator == 0)
+       result = "0";
+     else 
+        result = numerator + "/" + denominator;
+       
     }
     return result;
   }
